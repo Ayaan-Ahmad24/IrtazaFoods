aba18e39f46dc2d1e41e6685592c7314d0a391bf
CodeMaster1122 <ayaansheikh061@gmail.com>
2024-09-12 20:47:04 +0500

    app.jsx updated

--- frontend/src/Components/Errorboundary.jsx
+++ frontend/src/Components/Errorboundary.jsx
@@ -1,34 +1,3 @@
-// import React, { Component } from 'react';
-
-// class ErrorBoundary extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = { hasError: false };
-//     }
-
-//     static getDerivedStateFromError() {
-//         // Update state so the next render will show the fallback UI
-//         return { hasError: true };
-//     }
-
-//     componentDidCatch(error, errorInfo) {
-//         // You can also log error messages to an error reporting service
-//         console.error('ErrorBoundary caught an error:', error, errorInfo);
-//     }
-
-//     render() {
-//         if (this.state.hasError) {
-//             // You can render any custom fallback UI
-//             return <h1>Something went wrong. Please try again later.</h1>;
-//         }
-
-//         return this.props.children; 
-//     }
-// }
-
-// export default ErrorBoundary;
-
-// src/Components/ErrorBoundary.jsx
 import React, { Component } from 'react';
 
 class ErrorBoundary extends Component {
